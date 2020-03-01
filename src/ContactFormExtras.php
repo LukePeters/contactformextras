315be0bed89118a00c48e56be9eb5ee2ac109e7f
Luke Peters <lukepeters@gmail.com>
2020-03-01 17:53:31 -0500

    Polished up module files

---
+++ src/ContactFormExtras.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Contact Form Extras module for Craft CMS 3.x
+ *
+ * Lets you to use a custom email template with the Craft CMS Contact Form plugin.
+ *
+ * @link      https://lukepeters.me
+ * @copyright Copyright (c) 2020 Luke Peters
+ */
+
+namespace modules\contactformextras;
+
+use modules\contactformextras\assetbundles\contactformextras\ContactFormExtrasAsset;
+
+use Craft;
+use craft\events\RegisterTemplateRootsEvent;
+use craft\events\TemplateEvent;
+use craft\i18n\PhpMessageSource;
+use craft\web\View;
+
+use yii\base\Event;
+use yii\base\InvalidConfigException;
+use yii\base\Module;
+
+use craft\contactform\events\SendEvent;
+use craft\contactform\Mailer;
+
+/**
+ *
+ * @author    Luke Peters
+ * @package   ContactFormExtras
+ * @since     1.0.0
+ *
+ */
+class ContactFormExtras extends Module
+{
+    // Static Properties
+    // =========================================================================
+
+    /**
+     * Static property that is an instance of this module class so that it can be accessed via
+     * ContactFormExtras::$instance
+     *
+     * @var ContactFormExtras
+     */
+    public static $instance;
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct($id, $parent = null, array $config = [])
+    {
+        Craft::setAlias('@modules/contactformextras', $this->getBasePath());
+        $this->controllerNamespace = 'modules\contactformextras\controllers';
+
+        // Translation category
+        $i18n = Craft::$app->getI18n();
+        /** @noinspection UnSafeIsSetOverArrayInspection */
+        if (!isset($i18n->translations[$id]) && !isset($i18n->translations[$id.'*'])) {
+            $i18n->translations[$id] = [
+                'class' => PhpMessageSource::class,
+                'sourceLanguage' => 'en-US',
+                'basePath' => '@modules/contactformextras/translations',
+                'forceTranslation' => true,
+                'allowOverrides' => true,
+            ];
+        }
+
+        // Set this as the global instance of this module class
+        static::setInstance($this);
+
+        parent::__construct($id, $parent, $config);
+    }
+
+    public function init()
+    {
+        parent::init();
+        self::$instance = $this;
+
+        // Intercept the email before it sends
+        Event::on(Mailer::class, Mailer::EVENT_BEFORE_SEND, function(SendEvent $e) {
+
+            // Get the form values
+            $name = $e->submission->fromName;
+            $email = $e->submission->fromEmail;
+            $subject = $e->submission->subject;
+            $message = $e->submission->message;
+            
+            // Load the custom email template
+            $customEmailTemplate = file_get_contents(__DIR__ . "/template.html");
+
+            // Insert form values into the custom email template
+            $customEmailTemplate = str_replace('[[name]]', $name, $customEmailTemplate);
+            $customEmailTemplate = str_replace('[[email]]', $email, $customEmailTemplate);
+            $customEmailTemplate = str_replace('[[subject]]', $subject, $customEmailTemplate);
+            $customEmailTemplate = str_replace('[[message]]', $message, $customEmailTemplate);
+            
+            // Replace the email text body and HTML body with our custom template
+            $e->message->setHtmlBody($customEmailTemplate);
+        });
+
+        Craft::info(
+            Craft::t(
+                'contact-form-extras',
+                '{name} module loaded',
+                ['name' => 'Contact Form Extras']
+            ),
+            __METHOD__
+        );
+    }
+}
---
+++ src/translations/en/contact-form-extras-module.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Contact Form Extras module for Craft CMS 3.x
+ *
+ * Adds a few features to the Craft CMS Contact Form plugin.
+ *
+ * @link      https://lukepeters.me
+ * @copyright Copyright (c) 2020 Luke Peters
+ */
+
+/**
+ * Contact Form Extras en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('contact-form-extras-module', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Luke Peters
+ * @package   ContactFormExtrasModule
+ * @since     1.0.0
+ */
+return [
+    'Contact Form Extras plugin loaded' => 'Contact Form Extras plugin loaded',
+];
